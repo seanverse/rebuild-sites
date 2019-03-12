079dec9d8c0580a27be229819dfbe338e92732c7
FangfangZhao <zhaofang123@gmail.com>
2019-03-12 12:51:32 +0800

    new pages

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,23 @@
 var express = require('express')
 var router = express.Router()
 
-/* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', {
     title: 'REBUILD - 免费开源高度可定制化的企业管理系统',
+    pretty: false
+  })
+})
+
+router.get('/download', function (req, res, next) {
+  res.render('download', {
+    title: '下载 REBUILD',
+    pretty: false
+  })
+})
+
+router.get('/about', function (req, res, next) {
+  res.render('about', {
+    title: '关于我们',
     pretty: false
   })
 })
