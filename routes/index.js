b893862a377e63464f6be6ed02f605a9c71bbdfd
FangfangZhao <zhaofang123@gmail.com>
2019-03-14 15:50:33 +0800

    lng

--- routes/index.js
+++ routes/index.js
@@ -5,22 +5,21 @@ router.get('/', function (req, res) {
   res.render('index', {
     title: req.t('home.title'),
     keywords: req.t('home.keywords'),
-    description: req.t('home.description'),
-    pretty: true
+    description: req.t('home.description')
   })
 })
 
 router.get('/download', function (req, res) {
   res.render('download', {
     title: '下载 REBUILD',
-    pretty: false
+    pretty: true
   })
 })
 
 router.get('/about', function (req, res) {
   res.render('about', {
     title: '关于我们',
-    pretty: false
+    pretty: true
   })
 })
 
