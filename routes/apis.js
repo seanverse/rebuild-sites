5865c516760c36527d8a8994f673aa40efa18e6c
FangfangZhao <zhaofang123@gmail.com>
2019-08-07 16:38:52 +0800

    API

--- routes/apis.js
+++ routes/apis.js
@@ -1,12 +1,40 @@
 const pupp = require('puppeteer')
 const express = require('express')
 const router = express.Router()
+const os = require('os')
+
+const URL_REGEX = /^(http|https)\:\/\/[a-z0-9\-\.]+(:[0-9]*)?\/?([a-z0-9\-\._\?\,\'\/\\\+&amp;%\$#\=~!:])*$/i
+
+// 检查授权
+function checkAuth(req) {
+  if (process.env.NODE_ENV === 'development') return true
+  return (req.headers.referer || '').includes('getrebuild.com')
+    || req.query.k === 'IjkMHgq94T7s7WkP'
+}
+
+// 发送文件
+function sendFile(res, attname, file) {
+  let options = {}
+  if (attname) {
+    options = {
+      headers: {
+        'Content-Type': 'application/octet-stream;charset=UTF-8',
+        'Content-Disposition': 'attachment; filename=' + attname
+      }
+    }
+  }
+  res.sendFile(file, options)
+}
 
 // 网页导出 PDF
 router.get('/pupp/pdf', async function (req, res) {
+  if (!checkAuth(req)) {
+    res.status(403).send({ error: 'Access forbidden' })
+    return
+  }
   const url = req.query.url
-  if (!url) {
-    res.send('无效参数 : ' + url)
+  if (!url || !URL_REGEX.test(url)) {
+    res.status(400).send({ error: 'Invalid parameter [url]' })
     return
   }
 
@@ -16,41 +44,51 @@ router.get('/pupp/pdf', async function (req, res) {
   })
   const page = await browser.newPage()
   await page.goto(url)
-  let dest = `/tmp/${new Date().getTime()}.pdf`
+
+  const dest = `${os.tmpdir()}/pupp-${new Date().getTime()}.pdf`
   await page.pdf({
     format: 'A4',
+    printBackground: true,
     path: dest
   })
-  console.log('Export dest : ' + dest)
   await browser.close()
 
-  res.sendFile(dest)
+  sendFile(res, req.query.attname, dest)
 })
 
 // 网页截图
 router.get('/pupp/screenshot', async function (req, res) {
+  if (!checkAuth(req)) {
+    res.status(403).send({ error: 'Access forbidden' })
+    return
+  }
   const url = req.query.url
-  if (!url) {
-    res.send('无效参数 : ' + url)
+  if (!url || !URL_REGEX.test(url)) {
+    res.status(400).send({ error: 'Invalid parameter [url]' })
     return
   }
 
-  const params = req.query.params || '0,0,0,0,0,0'  // width,height,x0,x1,y0,y1
-  
+  const wh = (req.query.wh || '').split(',')
+  let viewport = { deviceScaleFactor: 1, width: 1280, height: 720 }
+  if (!isNaN(wh[0]) && ~~wh[0] > 0) viewport.width = ~~wh[0]
+  if (!isNaN(wh[1]) && ~~wh[1] > 0) viewport.height = ~~wh[1]
+
   const browser = await pupp.launch({
     headless: true,
     slowMo: 0
   })
   const page = await browser.newPage()
+  if (viewport) await page.setViewport(viewport)
+
   await page.goto(url)
-  let dest = `/tmp/${new Date().getTime()}.png`
+  let dest = `${os.tmpdir()}/pupp-${new Date().getTime()}.png`
   await page.screenshot({
-    path: dest
+    path: dest,
+    fullPage: req.query.fp == 1
   })
-  console.log('Export dest : ' + dest)
   await browser.close()
-  
-  res.sendFile(dest)
+
+  sendFile(res, req.query.attname, dest)
 })
 
 module.exports = router
