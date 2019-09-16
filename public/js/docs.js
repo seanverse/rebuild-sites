011143fc81b40d4763c6a0964aebf39440220ec7
devezhao-mbp <zhaofang123@gmail.com>
2019-09-16 22:50:18 +0800

    Update docs.js

--- public/js/docs.js
+++ public/js/docs.js
@@ -4,7 +4,7 @@
 
   $('<a title="Edit on GitHub" target="_blank" href="https://github.com/getrebuild/website/edit/master' + location.pathname + '.md"><i class="fa fa-github fa-fw"></i></a>').appendTo('.markdown-section>h1')
 
-  $('..markdown-section p>img').each(function () {
+  $('.markdown-section p>img').each(function () {
     $(this).parent().addClass('text-center')
   })
 
